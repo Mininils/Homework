fe962fb917732dceaccf0adae3118132e843df60
Vitaliy <147307642+Mininils@users.noreply.github.com>
2024-09-16 21:10:32 +0300

    Add files via upload

---
+++ DZLab3/HomeworkLab3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <locale.h>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	getchar();
+	task1();
+}
+
+#define F 2.2
+#define KG 0.45
+
+int task1()
+{
+	int a;
+	puts("¬ведите кол-во фунтов");
+	scanf("%d", &a);
+	printf("%d фунтов - это %0.3f кг", a, a * KG );
+	printf("\n%d фунтов - это %0.3f граммов", a, a * KG / 1000);
+	printf("\n%d фунтов - это %0.3f тонн", a, a * KG * 1000);
+}
